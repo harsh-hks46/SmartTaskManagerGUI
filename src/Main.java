aafa6bf776a70faf142a0d410fac6768a5a2dc06
harsh-hks46 <harsh.24bai10262@vitbhopal.ac.in>
2026-03-29 12:12:51 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,13 @@ import com.formdev.flatlaf.FlatDarkLaf;
 
 public class Main {
     public static void main(String[] args) {
-
-        // Set modern look and feel
+// Entry point of SmartTask Manager application
         try {
             FlatDarkLaf.setup();
         } catch (Exception e) {
             System.out.println("Failed to initialize FlatLaf");
         }
 
-        // Run GUI on Event Dispatch Thread (important for Swing)
         SwingUtilities.invokeLater(() -> new MainFrame());
     }
 }
